cb253c186dfc258b6b5c307bf9a70fc44c03db7c
Archibaka <ss.beaver@yandex.ru>
2025-07-21 18:20:55 +0300

    Add files via upload

---
+++ conv.py
@@ -0,0 +1,10 @@
+def build_translation_dict():
+    result = {}
+    QWERTY = "qwertyuiop[]asdfghjkl;'zxcvbnm`,."
+    JCUKEN = "йцукенгшщзхъфывапролджэячсмитьёбю"
+    for (latin, cyrillic) in zip(QWERTY, JCUKEN):
+        result[ord(latin)] = cyrillic
+        # Handle letter case pairs
+        if latin.islower():
+            result[ord(latin.upper())] = cyrillic.upper()
+    return result
